a05bcf7d523ca201a21116228279c200230b8881
brayanduranvelasquez <brayan13s133@gmail.com>
2022-08-29 12:44:02 -0400

    brayan13s133 MOD seccion contactar
    La seccion de contactar fue modificada por dos botones, correspondidos a contactar por WS o por Gmail

--- js/script.js
+++ js/script.js
@@ -18,59 +18,6 @@ window.onscroll = () => {
   }
 };
 
-//////////////// EMAILJS ///////////////////////
-
-const btn = document.getElementById("button");
-const form = document.getElementById("form");
-emailjs.init("jzAVLBbhppWLY_Ob2");
-
-function enableButton() {
-  btn.value = "Enviar";
-  btn.disabled = false;
-}
-
-function toastSuccessAlert() {
-  halfmoon.initStickyAlert({
-    content: "Se ha enviado el correo exitosamente",
-    title: "Envio exitoso",
-    alertType: "alert-success",
-    fillType: "filled-lm",
-  });
-}
-
-function toastDangerAlert() {
-  halfmoon.initStickyAlert({
-    content: "Ha ocurrido un error al enviar el correo, intente más tarde",
-    title: "Falla en el envio",
-    alertType: "alert-danger",
-    fillType: "filled",
-  });
-}
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  btn.value = "Enviando...";
-  btn.disabled = true;
-
-  const serviceID = "default_service";
-  const templateID = "template_nvsfczu";
-
-  emailjs.sendForm(serviceID, templateID, this).then(
-    () => {
-      enableButton();
-      toastSuccessAlert();
-      form.reset();
-    },
-    (err) => {
-      enableButton();
-      form.reset();
-      toastDangerAlert();
-    }
-  );
-});
-
-
 //////////////// REDIRECT ///////////////////////
 
 function redirect(link) {
