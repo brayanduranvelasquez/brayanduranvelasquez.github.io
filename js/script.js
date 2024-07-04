e0b333d5a6dd02efc8c90ea7458dffa13fe22cf8
Brayan Durán <brayan13s133@gmail.com>
2024-07-04 16:14:50 -0400

    feat

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
-window.onload = () => {
-  setTimeout(() => {
-    document.getElementById("container-loader").style.display = "none";
-  }, 200);
-};
-
-//////////////// Anchor Top ///////////////////////
-
-let anchorTop = document.getElementById("anchorTop");
+let anchorTop = document.getElementById('anchorTop');
 
 window.onscroll = () => {
   let yPosition = window.scrollY;
 
   if (yPosition > 600) {
-    anchorTop.style.display = "flex";
+    anchorTop.style.display = 'flex';
   } else {
-    anchorTop.style.display = "none";
+    anchorTop.style.display = 'none';
   }
 };
 
 //////////////// REDIRECT ///////////////////////
 
 function redirect(link) {
-  window.open(link, "_blank");
+  window.open(link, '_blank');
 }
